2cff5dfc7046da0cd7e7946306ff41bfb6d1499d
Danielle <danielle@daniellemullane.com>
2015-05-06 16:00:35 -0600

    Add function to fill in canvas and call on canvas stroke function

--- canvas.js
+++ canvas.js
@@ -1,6 +1,14 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext('2d');
+var size = { x: canvas.width, y: canvas.height }
+
+function drawBorder(){
+  context.strokeRect(0,0, size.x, size.y);
+}
+
+function fillRectangle(){
+  context.fillStyle = 'blue';
+  context.fillRect(0, 0, size.x/2, size.y/2);
+}
+
+drawBorder();
